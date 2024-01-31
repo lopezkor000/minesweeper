86bd66f1693c49152ce1f2018a3de1b80c472ef3
Kory <koriellopez03@gmail.com>
2024-01-31 14:51:07 -0600

    Tile: given attributes

--- modules/tile.py
+++ modules/tile.py
@@ -1,2 +1,12 @@
 class Tile:
-	pass
+	def __init__(self, pos, is_mine=False):
+		# Tile Attr
+		self.pos = pos
+		self.is_mine = is_mine
+		self.surrounding = None
+		# Graphic Attr
+		self.size = None
+		self.color = None
+	
+	def draw(self):
+		pass
