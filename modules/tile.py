2e9a22dfbab1bc0ba175e5d91746c04f1edaa6f1
Kory <koriellopez03@gmail.com>
2024-02-07 14:16:42 -0600

    Tile given draw

--- modules/tile.py
+++ modules/tile.py
@@ -1,12 +1,15 @@
+import pygame as pg
+
 class Tile:
-	def __init__(self, pos, is_mine=False):
+	def __init__(self, screen, pos:tuple, is_mine:bool=False):
 		# Tile Attr
 		self.pos = pos
 		self.is_mine = is_mine
 		self.surrounding = None
 		# Graphic Attr
+		self.screen = screen
 		self.size = None
 		self.color = None
 	
 	def draw(self):
-		pass
+		pg.draw.rect(self.screen, self.color, self.pos + self.size)
